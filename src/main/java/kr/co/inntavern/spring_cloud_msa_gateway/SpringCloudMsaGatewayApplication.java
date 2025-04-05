5160fb10a9d5aeca5c58f0495a2d79e5c90ddbc0
co2plant <93631841+co2plant@users.noreply.github.com>
2025-04-05 14:49:11 +0900

    init: Chapter 9

---
+++ src/main/java/kr/co/inntavern/spring_cloud_msa_gateway/SpringCloudMsaGatewayApplication.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.spring_cloud_msa_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudMsaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/inntavern/spring_cloud_msa_gateway/SpringCloudMsaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inntavern.spring_cloud_msa_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMsaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
